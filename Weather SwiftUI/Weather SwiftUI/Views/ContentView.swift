fd64afb42cde8278037e6bb6d93ce510c20b020f
TheHabibi <celebialieren@gmail.com>
2022-06-24 18:52:16 +0200

    Initial Commit

---
+++ Weather SwiftUI/Weather SwiftUI/Views/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  Weather SwiftUI
+//
+//  Created by Ali Eren Çelebi on 24.06.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @StateObject var locationManager = LocationManager()
+    
+    var body: some View {
+        VStack {
+            WelcomeView()
+                .environmentObject(locationManager)
+        }
+        .background(.blue)
+        .preferredColorScheme(.dark)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Weather SwiftUI/Weather SwiftUI/Weather_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_SwiftUIApp.swift
+//  Weather SwiftUI
+//
+//  Created by Ali Eren Çelebi on 24.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct Weather_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
